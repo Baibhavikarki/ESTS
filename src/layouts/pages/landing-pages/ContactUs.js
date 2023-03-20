a5511f1953e5d31b9f85cdcb2745b9f6997b9dbe
Baibhavikarki <baibhavikarki325@gmail.com>
2023-03-20 12:24:22 -0400

    testing jenkins

--- src/layouts/pages/landing-pages/ContactUs.js
+++ src/layouts/pages/landing-pages/ContactUs.js
@@ -74,7 +74,7 @@ function ContactUs() {
               </MKBox>
               <MKBox p={3}>
                 <MKTypography variant="body2" color="text" mb={3}>
-                  For further question, including partnership opportunities, please email
+                  For further questions, please email
                   endangered-species@teampanda.com or contact using our contact form.
                 </MKTypography>
                 <MKBox width="100%" component="form" method="post" autocomplete="off">
